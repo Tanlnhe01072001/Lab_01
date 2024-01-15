ea538493650102d63cdc0786ab9ed8abc26f0a57
Nhat Tan <tanln01072001@gmail.com>
2024-01-15 23:10:22 +0700

    Design UI and write codes for WinForms project

---
+++ AutomobileWinApp/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutomobileLibrary.BussinessObject;
+using AutomobileLibrary.Repository;
+namespace AutomobileWinApp
+{
+    public partial class frmCarDetails : Form
+    {
+        public frmCarDetails()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
