7634997b1d3ccae6f7665aed8ec2a052ef584bbe
Nhat Tan <tanln01072001@gmail.com>
2024-01-17 15:31:43 +0700

    hoan thanh winform frmCarDetails

--- AutomobileWinApp/Form1.Designer.cs
+++ AutomobileWinApp/Form1.Designer.cs
@@ -28,18 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lbCarID = new System.Windows.Forms.Label();
+            lbCarName = new System.Windows.Forms.Label();
+            lbManufacturer = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            txtCarID = new System.Windows.Forms.TextBox();
+            txtCarName = new System.Windows.Forms.TextBox();
+            txtPrice = new System.Windows.Forms.MaskedTextBox();
+            maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            cboManufacturer = new System.Windows.Forms.ComboBox();
+            btnSave = new System.Windows.Forms.Button();
+            btnCancel = new System.Windows.Forms.Button();
             SuspendLayout();
+            // 
+            // lbCarID
+            // 
+            lbCarID.AutoSize = true;
+            lbCarID.Location = new System.Drawing.Point(30, 27);
+            lbCarID.Name = "lbCarID";
+            lbCarID.Size = new System.Drawing.Size(50, 20);
+            lbCarID.TabIndex = 0;
+            lbCarID.Text = "Car ID";
+            // 
+            // lbCarName
+            // 
+            lbCarName.AutoSize = true;
+            lbCarName.Location = new System.Drawing.Point(30, 65);
+            lbCarName.Name = "lbCarName";
+            lbCarName.Size = new System.Drawing.Size(75, 20);
+            lbCarName.TabIndex = 1;
+            lbCarName.Text = "Car Name";
+            // 
+            // lbManufacturer
+            // 
+            lbManufacturer.AutoSize = true;
+            lbManufacturer.Location = new System.Drawing.Point(30, 103);
+            lbManufacturer.Name = "lbManufacturer";
+            lbManufacturer.Size = new System.Drawing.Size(97, 20);
+            lbManufacturer.TabIndex = 2;
+            lbManufacturer.Text = "Manufacturer";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(30, 143);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(41, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Price";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(30, 184);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(88, 20);
+            label5.TabIndex = 4;
+            label5.Text = "ReleaseYear";
+            // 
+            // txtCarID
+            // 
+            txtCarID.Location = new System.Drawing.Point(140, 20);
+            txtCarID.Name = "txtCarID";
+            txtCarID.Size = new System.Drawing.Size(255, 27);
+            txtCarID.TabIndex = 5;
+            // 
+            // txtCarName
+            // 
+            txtCarName.Location = new System.Drawing.Point(140, 65);
+            txtCarName.Name = "txtCarName";
+            txtCarName.Size = new System.Drawing.Size(255, 27);
+            txtCarName.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new System.Drawing.Point(140, 140);
+            txtPrice.Mask = "000000000";
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new System.Drawing.Size(255, 27);
+            txtPrice.TabIndex = 7;
+            txtPrice.Text = "0";
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new System.Drawing.Point(140, 184);
+            maskedTextBox2.Mask = "0000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new System.Drawing.Size(255, 27);
+            maskedTextBox2.TabIndex = 8;
+            maskedTextBox2.Text = "0";
+            // 
+            // cboManufacturer
+            // 
+            cboManufacturer.FormattingEnabled = true;
+            cboManufacturer.Items.AddRange(new object[] { "Audi", "BMW", "Ford", "Honda", "Hyundai", "Kia", "Suzuki", "Toyota" });
+            cboManufacturer.Location = new System.Drawing.Point(140, 106);
+            cboManufacturer.Name = "cboManufacturer";
+            cboManufacturer.Size = new System.Drawing.Size(255, 28);
+            cboManufacturer.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            btnSave.Location = new System.Drawing.Point(154, 248);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(94, 29);
+            btnSave.TabIndex = 10;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            btnCancel.Location = new System.Drawing.Point(290, 248);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new System.Drawing.Size(94, 29);
+            btnCancel.TabIndex = 11;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
             // 
             // frmCarDetails
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
+            ClientSize = new System.Drawing.Size(453, 297);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(cboManufacturer);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(txtPrice);
+            Controls.Add(txtCarName);
+            Controls.Add(txtCarID);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(lbManufacturer);
+            Controls.Add(lbCarName);
+            Controls.Add(lbCarID);
             Name = "frmCarDetails";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "frmCarDetails";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbCarID;
+        private System.Windows.Forms.Label lbCarName;
+        private System.Windows.Forms.Label lbManufacturer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCarID;
+        private System.Windows.Forms.TextBox txtCarName;
+        private System.Windows.Forms.MaskedTextBox txtPrice;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.ComboBox cboManufacturer;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
